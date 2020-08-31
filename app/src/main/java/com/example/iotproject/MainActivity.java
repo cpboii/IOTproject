e923f50630f0a63f427eca71a1d7ca6d689c64d5
cpboii <56568332+cpboii@users.noreply.github.com>
2020-09-01 04:19:14 +0800

    123

--- app/src/main/java/com/example/iotproject/MainActivity.java
+++ app/src/main/java/com/example/iotproject/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.iotproject;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -14,57 +17,90 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+
+
 public class MainActivity extends AppCompatActivity {
-    TextView a,b,c,d;
+    TextView humidity,temperature,level,moistureView,lastWaterDate;
     DatabaseReference mDatabase;
-    Button btn;
+
+    Button btn, highButton,averageButton,lowButton;
     private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        a=(TextView)findViewById(R.id.humid);
-        b=(TextView)findViewById(R.id.temp);
-        c=(TextView)findViewById(R.id.text);
-        d=(TextView)findViewById(R.id.moisture);
+        humidity=(TextView)findViewById(R.id.humid);
+        temperature=(TextView)findViewById(R.id.temp);
+        lastWaterDate=(TextView)findViewById(R.id.lastDate);
+        btn = (Button) findViewById(R.id.btn);
+        highButton = (Button) findViewById(R.id.button5);
+        averageButton = (Button) findViewById(R.id.button6);
+        lowButton = (Button) findViewById(R.id.button7);
+
+
 
                 mDatabase = FirebaseDatabase.getInstance().getReference().child("asgm");
                 mDatabase.addValueEventListener(new ValueEventListener() {
                     @Override
                     public void onDataChange(DataSnapshot dataSnapshot) {
-                        // This method is called once with the initial value and again
-                        // whenever data at this location is updated.
+
                         String humid=dataSnapshot.child("Humidity").getValue().toString();
                         String temp=dataSnapshot.child("Temp").getValue().toString();
-                        String text=dataSnapshot.child("Text").getValue().toString();
-                        String moisture=dataSnapshot.child("Moisture").getValue().toString();
-                        a.setText(humid);
-                        b.setText(temp);
-                        c.setText(text);
-                        d.setText(moisture);
+                        String moisture=dataSnapshot.child("moisture level").getValue().toString();
+                        String lastDate=dataSnapshot.child("last watering").getValue().toString();
+                        humidity.setText(humid);
+                        temperature.setText(temp);
+                        lastWaterDate.setText(lastDate);
 
+                        highButton.setClickable(false);
+                        averageButton.setClickable(false);
+                        lowButton.setClickable(false);
+
+                        if(moisture.equals("High")){
+                            highButton.setEnabled(true);
+                            averageButton.setEnabled(false);
+                            lowButton.setEnabled(false);
+                            btn.setEnabled(false);
+                       }else if (moisture.equals("Average")){
+                            highButton.setEnabled(false);
+                            averageButton.setEnabled(true);
+                            lowButton.setEnabled(false);
+                            btn.setEnabled(true);
+                        }else if(moisture.equals("Low")){
+                            highButton.setEnabled(false);
+                            averageButton.setEnabled(false);
+                            lowButton.setEnabled(true);
+                            btn.setEnabled(true);
+                        }
                     }
+
 
                     @Override
                     public void onCancelled(DatabaseError error) {
-                        // Failed to read value
+
                         Log.w(TAG, "Failed to read value.", error.toException());
+
                     }
                 });
 
 
 
+                btn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
 
+                        mDatabase.child("led").setValue("1");
+                        openDialog();
 
-
-
-
-
-
-        // Read from the database
+                    }
+                });
 
     }
 
+public void openDialog(){
+        msgDialog message = new msgDialog();
+        message.show(getSupportFragmentManager(), "message dialog");
+}
 
 
 
---
+++ app/src/main/java/com/example/iotproject/msgDialog.java
@@ -0,0 +1,24 @@
+package com.example.iotproject;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatDialogFragment;
+
+public class msgDialog extends AppCompatDialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Successful")
+                .setMessage("You have water your plant successfully!")
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+        return builder.create();
+    }
+}
